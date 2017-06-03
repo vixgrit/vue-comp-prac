ed89c151b7cc559dc7db2d1e6724f0747d629096
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-03 14:17:15 -0400

    register components locally vs. globally

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-Vue.component('my-cmp', {
+// var data = {
+//   status: 'Critical'
+// }
+
+var cmp = {
   data: function() {
     return {
       status: 'Critical'
     }
   },
-  template: '<p>Server Status: {{ status}}</p>'
+  template: '<p>Server Status: {{ status}} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
+  }
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    'my-cmp': cmp
+  }
 })
 
 new Vue({
-  el: '#app'
+  el: '#app2',
+  components: {
+    'myCmpTwo': cmp
+  }
 })
